a518b1e76602ede76fa26010cc19419d41432cd3
Ignacio Quevedo <jignacioquevedo@gmail.com>
2023-10-30 22:20:14 -0300

    preentrega3 - estructura html

--- main.js
+++ main.js
@@ -1,142 +1,133 @@
-function Persona(nombre, apellido, email) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
-    this.prestamos = [];
-}
-
-function simuladorPrestamo(usuario) {
-    let solicitarPrestamo = true;
-    let cuotaMensual;
-    let totalDevolver;
+document.addEventListener('DOMContentLoaded', function() {
     
-    do{
-        const tipoPrestamo = prompt("Seleccione el tipo de préstamo:\n1. 12 meses\n2. 18 meses\n3. 36 meses").toLowerCase();
+    const nombreInput = document.getElementById('nombre');
+    const apellidoInput = document.getElementById('apellido');
+    const emailInput = document.getElementById('email');
+    const tipoPrestamoSelect = document.getElementById('tipoPrestamo');
+    const montoInput = document.getElementById('monto');
+    const calcularBtn = document.getElementById('calcularBtn');
+    const resultadoDiv = document.getElementById('resultadoDiv');
+    const solicitarPrestamoBtn = document.getElementById('solicitarPrestamoBtn');
+    const mostrarHistorialBtn = document.getElementById('mostrarHistorialBtn');
+    const formContainer = document.getElementById('form-container');
+    const historialContainer = document.getElementById('historial-container');
+    const historial = document.getElementById('historial');
+    const borrarHistorialBtn = document.getElementById('borrarHistorialBtn');
+
+    let prestamos = JSON.parse(localStorage.getItem('prestamos')) || [];
+
+    solicitarPrestamoBtn.addEventListener('click', function() {
+        formContainer.style.display = 'block';
+        historialContainer.style.display = 'none';
+        resultadoDiv.innerHTML = '';
+    });
+
+    mostrarHistorialBtn.addEventListener('click', function() {
+        formContainer.style.display = 'none';
+        historialContainer.style.display = 'block';
+        mostrarHistorialPrestamos();
+    });
+
+    borrarHistorialBtn.addEventListener('click', function() {
+        localStorage.removeItem('prestamos');
+        
+        historial.innerHTML = '<p>Historial borrado.</p>';
+    });
+
+    calcularBtn.addEventListener('click', function() {
+        
+        const nombre = nombreInput.value;
+        const apellido = apellidoInput.value;
+        const email = emailInput.value;
+        const tipoPrestamo = tipoPrestamoSelect.value;
+        const monto = parseFloat(montoInput.value);
+
         let tasaInteres = 0;
         let cuotas = 0;
 
         switch (tipoPrestamo) {
-            case "1":
-            case "12 meses":
+            case "12":
                 tasaInteres = 1.29;
                 cuotas = 12;
                 break;
-            case "2":
-            case "18 meses":
+            case "18":
                 tasaInteres = 1.76; 
                 cuotas = 18;
                 break;
-            case "3":
-            case "36 meses":
+            case "36":
                 tasaInteres = 2.17; 
                 cuotas = 36;
                 break;
             default:
-                alert("Opción no válida. Seleccione nuevamente una opción.");
-                continue; 
+                alert("Opcion no valida. Seleccione nuevamente una opcion.");
+                return;
         }
 
-        const montoPrestamo = parseFloat(prompt("Ingrese el monto del préstamo:"));
-
-        if (isNaN(montoPrestamo) || montoPrestamo <= 0) {
-            alert("El monto del préstamo debe ser un número positivo.");
-            continue; 
-        }
-    
         function calculoInteres(montoPrestamo, tasaInteres){
-            let interesTotal = montoPrestamo * tasaInteres;
-            return interesTotal;
+            return montoPrestamo * tasaInteres;
         }
 
         function totalADevolver(montoPrestamo, tasaInteres){
-            let interes = calculoInteres(montoPrestamo, tasaInteres);
-            let montoTotal = montoPrestamo + interes;
-            return montoTotal;
+            const interes = calculoInteres(montoPrestamo, tasaInteres);
+            return montoPrestamo + interes;
         }
 
-        function calculoCuotas(montoPrestamo,tasaInteres,cuotas){
-            let total = totalADevolver(montoPrestamo,tasaInteres);
-            let cuotaMensual = total / cuotas;
+        function calculoCuotas(montoPrestamo, tasaInteres, cuotas){
+            const total = totalADevolver(montoPrestamo, tasaInteres);
+            const cuotaMensual = total / cuotas;
             return cuotaMensual;
-        }      
-        
-        function resumenPrestamo(usuario, tipoPrestamo, montoPrestamo, tasaInteres,cuotas){
-            let cuotaCalculada = calculoCuotas(montoPrestamo, tasaInteres, cuotas);
-            let totalDevolver = totalADevolver(montoPrestamo, tasaInteres);           
-
-            let resumen= "Resumen del prestamo \n"+
-            `Nombre: ${usuario.nombre}\n` +
-            `Apellido: ${usuario.apellido}\n` +
-            `Correo: ${usuario.email}\n` +
-            `Tipo de prestamo: ${tipoPrestamo}\n`+
-            `Monto solicitado: $${montoPrestamo}\n`+
-            `Tasa de interes: ${(tasaInteres * 100).toFixed(2)}%\n`+
-            `Cantidad de cuotas: ${cuotas}\n`+
-            `Monto cuota mensual: $${cuotaCalculada.toFixed(2)}\n`+
-            `Total a devolver: $${totalDevolver.toFixed(2)}`;
-
-            alert(resumen);
         }
 
-
-        resumenPrestamo(usuario,tipoPrestamo,montoPrestamo,tasaInteres,cuotas);
-
-        if (usuario.prestamos.length >= 5) {
-            alert("Ha llegado al límite de préstamos solicitados. No puede solicitar más por el momento.");
-            solicitarPrestamo = false;
-        } else {
-        const cuotaMensual = calculoCuotas(montoPrestamo, tasaInteres, cuotas);
-        const totalDevolver = totalADevolver(montoPrestamo, tasaInteres);
-
-        //Agregamos el prestamo pedido al array
-        usuario.prestamos.push({
-            tipo: tipoPrestamo,
-            monto: montoPrestamo,
+        const cuotaMensual = calculoCuotas(monto, tasaInteres, cuotas);
+        const totalDevolver = totalADevolver(monto, tasaInteres);
+        
+        const prestamo = {
+            nombre: nombre,
+            apellido: apellido,
+            email: email,
+            tipoPrestamo: tipoPrestamo,
+            monto: monto,
             tasa: tasaInteres,
             cuotas: cuotas,
-            cuotaMensual: cuotaMensual,
-            totalDevolver: totalDevolver
-        });
-
-        function mostrarHistorialPrestamos(usuario) {
-            console.log(`Historial de prestamos para ${usuario.nombre} ${usuario.apellido} (${usuario.email}):`);
-            
-            usuario.prestamos.forEach((prestamo, index) => {
-                console.log(`\nPrestamo ${index + 1}:`);
-                console.log(`Tipo de prestamo: ${prestamo.tipo}`);
-                console.log(`Monto solicitado: $${prestamo.monto}`);
-                console.log(`Tasa de interes: ${(prestamo.tasa * 100).toFixed(2)}%`);
-                console.log(`Cantidad de cuotas: ${prestamo.cuotas}`);
-                console.log(`Monto de la cuota mensual: $${prestamo.cuotaMensual.toFixed(2)}`);
-                console.log(`Total a devolver: $${prestamo.totalDevolver.toFixed(2)}`);
-            });
-        }
+            cuotaMensual: cuotaMensual.toFixed(2),
+            totalDevolver: totalDevolver.toFixed(2)
+        };
         
-        let opcionValida = false;
-            while (!opcionValida) {
-                const otraSolicitud = prompt("¿Desea solicitar otro prestamo? (Si/No)").toLowerCase();
-                if (otraSolicitud === "si") {
-                    opcionValida = true;
-                    solicitarPrestamo = true;
-                } else if (otraSolicitud === "no") {
-                    opcionValida = true;
-                    solicitarPrestamo = false;
-                } else {
-                    alert("Opción no valida. Por favor, seleccione 'Si' o 'No'.");
-                }
-            }
-        }
-    }while(solicitarPrestamo);
+        const prestamos = JSON.parse(localStorage.getItem('prestamos')) || [];
 
-    alert("Gracias por usar nuestro servicio. ¡Hasta pronto!");
+        prestamos.push(prestamo);
+        
+        localStorage.setItem('prestamos', JSON.stringify(prestamos));
+        
+        mostrarResultadoPrestamo(nombre, apellido, email, tipoPrestamo, monto, tasaInteres, cuotas, cuotaMensual, totalDevolver);;
+    });
+    
+    function mostrarResultadoPrestamo(nombre, apellido, email, tipoPrestamo, monto, tasaInteres, cuotas, cuotaMensual, totalDevolver){
+        resultadoDiv.innerHTML = `<p>Prestamo solicitado correctamente.</p>
+                                <p>Resumen:</p>
+                                <p>Tipo de Préstamo: ${tipoPrestamoSelect.value} meses</p>
+                                <p>Monto Solicitado: $${montoInput.value}</p>
+                                <p>Tasa de Interés: ${(tasaInteres * 100).toFixed(2)}%</p>
+                                <p>Cantidad de Cuotas: ${cuotas}</p>
+                                <p>Cuota Mensual: $${cuotaMensual.toFixed(2)}</p>
+                                <p>Total a Devolver: $${totalDevolver.toFixed(2)}</p>`;
+                            }
 
-    mostrarHistorialPrestamos(usuario);
-}
-
-//Solicitamos el nombre de la persona que va a solicitar el prestamo
-const nombre = prompt("Ingrese su nombre:");
-const apellido = prompt("Ingrese su apellido:");
-const email = prompt("Ingrese su correo electrónico:");
-const usuario = new Persona(nombre, apellido, email);
-
-simuladorPrestamo(usuario);
+    function mostrarHistorialPrestamos() {
+        historial.innerHTML = '';
+            prestamos.forEach((prestamo, index) => {
+                const prestamoDiv = document.createElement('div');
+                prestamoDiv.innerHTML = `
+                <p>Prestamo ${index + 1}:</p>
+                <p>Nombre: ${prestamo.nombre} ${prestamo.apellido}</p>
+                <p>Email: ${prestamo.email}</p>
+                <p>Tipo de Préstamo: ${prestamo.tipoPrestamo} meses</p>
+                <p>Monto Solicitado: $${prestamo.monto}</p>
+                <p>Tasa de Interés: ${(prestamo.tasa * 100).toFixed(2)}%</p>
+                <p>Cantidad de Cuotas: ${prestamo.cuotas}</p>
+                <p>Cuota Mensual: $${prestamo.cuotaMensual}</p>
+                <p>Total a Devolver: $${prestamo.totalDevolver}</p>`;
+                        historial.appendChild(prestamoDiv);
+            });
+    }
+});
